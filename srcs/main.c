279d2cb64fd7241ed1df80090591c2453da586bf
Philippe Menard <phil.menard1668@gmail.com>
2025-05-23 12:54:41 +0200

    added parsing for number of arguments

--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,15 @@
 #include "../cub3d.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
+	if (argc < 2)
+	{
+		printf("An argument including a map in .cub format is required.\n");
+		return (1);
+	}
+	else if (argc > 2)
+		return (printf("Too many arguments.\n"), (1));
+	else
+		printf("%s\n", argv[1]);
 	return (0);
 }
